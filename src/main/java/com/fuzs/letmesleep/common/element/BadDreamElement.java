a850f36281f7ab0474bd3ac3bb3a9172ecd714a8
Fuzss <mrfusseel@gmail.com>
2021-01-09 22:22:23 +0100

    prepare for first beta release

--- src/main/java/com/fuzs/letmesleep/common/element/BadDreamElement.java
+++ src/main/java/com/fuzs/letmesleep/common/element/BadDreamElement.java
@@ -8,6 +8,7 @@ import com.google.common.collect.Lists;
 import net.minecraft.block.BedBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
+import net.minecraft.command.arguments.EntityAnchorArgument;
 import net.minecraft.entity.*;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
@@ -18,11 +19,15 @@ import net.minecraft.util.Direction;
 import net.minecraft.util.EntityPredicates;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.spawner.WorldEntitySpawner;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -63,12 +68,12 @@ public class BadDreamElement extends AbstractElement implements ISidedElement.Co
     @Override
     public void setupCommonConfig(ForgeConfigSpec.Builder builder) {
 
-        addToConfig(builder.comment("Chance to spawn a monster. Higher values make it more likely to happen.").defineInRange("Spawn Monster Chance", 12, 0, 256), v -> this.spawnMonsterChance = v);
-        addToConfig(builder.comment("Range to spawn a monster in from the bed.").defineInRange("Spawn Monster Range", 2, 0, 16), v -> this.spawnMonsterRange = v);
+        addToConfig(builder.comment("Chance to spawn a monster. Higher values make it more likely to happen.").defineInRange("Spawn Monster Chance", 8, 0, 128), v -> this.spawnMonsterChance = v);
+        addToConfig(builder.comment("Range to spawn a monster in from the bed. Increasing the range makes it a lot less likely for a monster to spawn.").defineInRange("Spawn Monster Range", 2, 0, 16), v -> this.spawnMonsterRange = v);
         addToConfig(builder.comment("Possible monsters to spawn. One entry is chosen at random.", EntryCollectionBuilder.CONFIG_STRING_BUILDER.apply(",<weight>")).define("Spawnable Monsters", Lists.newArrayList(getEntityAndWeight(EntityType.ZOMBIE, 2), getEntityAndWeight(EntityType.SKELETON, 1), getEntityAndWeight(EntityType.SPIDER, 1))),v -> {
 
-            BiPredicate<EntityType<?>, double[]> predicate = (entry, value) -> entry.getClassification() == EntityClassification.MONSTER && value.length == 1;
-            Map<EntityType<?>, double[]> entries = new EntryCollectionBuilder<>(ForgeRegistries.ENTITIES).buildEntryMap(v, predicate , "Not a monster or no weight");
+            BiPredicate<EntityType<?>, double[]> predicate = (entry, value) -> entry.getClassification() == EntityClassification.MONSTER && value.length == 1 && value[0] > 0;
+            Map<EntityType<?>, double[]> entries = new EntryCollectionBuilder<>(ForgeRegistries.ENTITIES).buildEntryMap(v, predicate , "Not a monster or incorrect weight");
             this.spawnableMonsters = entries.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, value -> (int) value.getValue()[0]));
         });
     }
@@ -81,15 +86,16 @@ public class BadDreamElement extends AbstractElement implements ISidedElement.Co
 
     private void onWorldTick(final TickEvent.WorldTickEvent evt) {
 
-        if (evt.phase != TickEvent.Phase.START || evt.world.isRemote) {
+        if (evt.phase != TickEvent.Phase.START || evt.world.isRemote || evt.world.getDifficulty() == Difficulty.PEACEFUL) {
 
             return;
         }
 
         ServerWorld world = (ServerWorld) evt.world;
-        if (((IServerWorldAccessor) world).getAllPlayersSleeping() && world.getPlayers().stream().noneMatch(player -> !player.isSpectator() && !player.isPlayerFullyAsleep())) {
+        List<ServerPlayerEntity> allPlayers = world.getPlayers();
+        if (((IServerWorldAccessor) world).getAllPlayersSleeping() && allPlayers.stream().noneMatch(player -> !player.isSpectator() && !player.isPlayerFullyAsleep())) {
 
-            this.performSleepSpawning(world, world.getPlayers());
+            this.performSleepSpawning(world, allPlayers);
         }
     }
 
@@ -102,56 +108,68 @@ public class BadDreamElement extends AbstractElement implements ISidedElement.Co
                 continue;
             }
 
-            int run = 0;
-            int attempts = 0;
-            for (int j = 0; j < 100; j++) {
+            AxisAlignedBB spawningBoundingBox = this.getSpawningBoundingBox(world, player);
+            if (spawningBoundingBox == null) {
 
-
-                AxisAlignedBB spawningBoundingBox = this.getSpawningBoundingBox(world, player);
-                int maxAttempts = world.getDifficulty().getId() * this.spawnMonsterChance;
-                for (int i = 0; i < maxAttempts; i++) {
-
-                    BlockPos spawnPos = this.getRandomPosWithinAABB(spawningBoundingBox, world.getRandom());
-                    Optional<EntityType<?>> entityType = this.getRandomEntity(world.getRandom());
-                    if (entityType.isPresent() && this.isSpawningPermitted(world, entityType.get(), spawnPos)) {
-
-                        Entity entity = entityType.get().create(world, null, null, null, spawnPos, SpawnReason.EVENT, false, false);
-                        if (entity instanceof MobEntity && this.setPathToPlayer(world, player, (MobEntity) entity)) {
-
-//                        // addEntityPassengers
-//                        world.func_242417_l(entity);
-//                        player.wakeUp();
-//                        player.lookAt(EntityAnchorArgument.Type.EYES, entity, EntityAnchorArgument.Type.EYES);
-//                        // spawn smoke particles
-//                        ((MobEntity) entity).spawnExplosionParticle();
-
-                            run++;
-                            attempts += i;
-                            entity.remove();
-                            break;
-                        } else if (entity != null) {
-
-                            entity.remove();
-                        }
-                    }
-                }
+                continue;
             }
 
-            System.out.println("Total: " + run + ", Average: " + (attempts / (double) run));
+            int maxAttempts = this.spawnMonsterChance * (int) Math.pow(3, world.getDifficulty().getId() - 1);
+            for (int index = 0; index < maxAttempts; index++) {
+
+                if (this.trySpawnMonster(world, player, spawningBoundingBox)) {
+
+                    break;
+                }
+            }
         }
     }
 
+    private boolean trySpawnMonster(ServerWorld world, ServerPlayerEntity player, AxisAlignedBB spawningBoundingBox) {
+
+        BlockPos pos = this.getRandomPosWithinAABB(spawningBoundingBox, world.getRandom());
+        Optional<EntityType<?>> entityType = this.getRandomEntity(world.getRandom());
+        if (entityType.isPresent() && this.isSpawningPermitted(world, entityType.get(), pos)) {
+
+            Entity entity = entityType.get().create(world);
+            if (entity instanceof MobEntity) {
+
+                entity.setLocationAndAngles(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5, MathHelper.wrapDegrees(world.getRandom().nextFloat() * 360.0F), 0.0F);
+                if (world.checkNoEntityCollision(entity) && world.hasNoCollisions(entity) && !world.containsAnyLiquid(entity.getBoundingBox()) && this.setPathToPlayer(world, player, (MobEntity) entity)) {
+
+                    this.setupSpawnedMonster((MobEntity) entity, world);
+                    player.wakeUp();
+                    player.lookAt(EntityAnchorArgument.Type.EYES, entity, EntityAnchorArgument.Type.EYES);
+
+                    return true;
+                } else {
+
+                    entity.remove();
+                }
+            }
+        }
+
+        return false;
+    }
+
     @SuppressWarnings("OptionalGetWithoutIsPresent")
+    @Nullable
     private AxisAlignedBB getSpawningBoundingBox(ServerWorld world, PlayerEntity player) {
 
         BlockPos bedPos = player.getBedPosition().get();
         BlockState state = world.getBlockState(bedPos);
-        Direction bedPosDirection = state.get(HorizontalBlock.HORIZONTAL_FACING);
-        // check for part, who knows what might happen
-        Direction otherPartDirection = state.get(BedBlock.PART) == BedPart.FOOT ? bedPosDirection : bedPosDirection.getOpposite();
+        // mods could add bed position which is not a bed
+        if (state.isBed(world, bedPos, player)) {
 
-        // expand by one in all positive directions as this side will be excluded when picking a random spot
-        return new AxisAlignedBB(bedPos, bedPos.offset(otherPartDirection)).grow(this.spawnMonsterRange).expand(1, 1, 1);
+            Direction bedPosDirection = state.get(HorizontalBlock.HORIZONTAL_FACING);
+            // check for part, who knows what might happen
+            Direction otherPartDirection = state.get(BedBlock.PART) == BedPart.FOOT ? bedPosDirection : bedPosDirection.getOpposite();
+
+            // expand by one in all positive directions as this side will be excluded when picking a random spot
+            return new AxisAlignedBB(bedPos, bedPos.offset(otherPartDirection)).grow(this.spawnMonsterRange).expand(1, 1, 1);
+        }
+
+        return null;
     }
 
     private BlockPos getRandomPosWithinAABB(AxisAlignedBB axisAlignedBB, Random random) {
@@ -169,7 +187,7 @@ public class BadDreamElement extends AbstractElement implements ISidedElement.Co
         for (Map.Entry<EntityType<?>, Integer> entry : this.spawnableMonsters.entrySet()) {
 
             weight -= entry.getValue();
-            if (weight <= 0) {
+            if (weight < 0) {
 
                 return Optional.of(entry.getKey());
             }
@@ -178,10 +196,11 @@ public class BadDreamElement extends AbstractElement implements ISidedElement.Co
         return Optional.empty();
     }
 
-    private boolean isSpawningPermitted(ServerWorld world, EntityType<?> entityType, BlockPos spawnPos) {
+    private boolean isSpawningPermitted(ServerWorld world, EntityType<?> entityType, BlockPos pos) {
 
-        boolean collisionCheck = world.hasNoCollisions(entityType.getBoundingBoxWithSizeApplied(spawnPos.getX(), spawnPos.getY(), spawnPos.getZ()));
-        return collisionCheck && EntitySpawnPlacementRegistry.canSpawnEntity(entityType, world, SpawnReason.EVENT, spawnPos, world.getRandom());
+        EntitySpawnPlacementRegistry.PlacementType placementType = EntitySpawnPlacementRegistry.getPlacementType(entityType);
+        boolean canSpawnAtLocation = WorldEntitySpawner.canCreatureTypeSpawnAtLocation(placementType, world, pos, entityType);
+        return canSpawnAtLocation && EntitySpawnPlacementRegistry.canSpawnEntity(entityType, world, SpawnReason.EVENT, pos, world.getRandom());
     }
 
     private boolean setPathToPlayer(ServerWorld world, ServerPlayerEntity player, MobEntity monster) {
@@ -200,6 +219,18 @@ public class BadDreamElement extends AbstractElement implements ISidedElement.Co
         }
 
         return false;
+    }
+
+    private void setupSpawnedMonster(MobEntity mobentity, ServerWorld world) {
+
+        mobentity.rotationYawHead = mobentity.rotationYaw;
+        mobentity.renderYawOffset = mobentity.rotationYaw;
+        mobentity.onInitialSpawn(world, world.getDifficultyForLocation(mobentity.getPosition()), SpawnReason.EVENT, null, null);
+        // add entity and passengers to world
+        world.func_242417_l(mobentity);
+        mobentity.playAmbientSound();
+        // spawn smoke particles
+        mobentity.spawnExplosionParticle();
     }
 
 }
